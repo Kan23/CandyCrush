c1075141d8cbd56741311a2e94cc78074a1f63e2
Kan23 <romain.lelievre93@gmail.com>
2016-01-22 14:40:23 +0100

    Update CandyCrushModel.java

--- CandyCrush/src/Game/CandyCrushModel.java
+++ CandyCrush/src/Game/CandyCrushModel.java
@@ -1,34 +1,42 @@
 package Game;
 class CandyCrushModel{
-	int grid[][] = new int[8][8];
+	private final int sizeGrid = 8; 
+	
+	int grid[][] = new int[sizeGrid][sizeGrid];
 	Integer score = 0;
-	boolean marked[][] = new boolean[8][8];
+	boolean marked[][] = new boolean[sizeGrid][sizeGrid];
     
 	public int getGridCase(int i, int j)
 	{
-		if(i >= 8 || j >= 8)
+		if(i >= sizeGrid || j >= sizeGrid)
 			return -1;
 		return grid[i][j];
 	}
 	
 	public boolean getMarkedCase(int i, int j)
 	{
-		if(i >= 8 || j >= 8)
+		if(i >= sizeGrid || j >= sizeGrid)
 			return false;
 		return marked[i][j];
 	}
 	
 	public void setGridCase(int i, int j, int value)
 	{
+		if(i >= sizeGrid || j >= sizeGrid)
+			return;
 		this.grid[i][j] = value;
 	}
 	
 	public void setMarkedCase(int i, int j, boolean value)
 	{
+		if(i >= sizeGrid || j >= sizeGrid)
+			return;
 		this.marked[i][j] = value;
 	}
 	
 	void swap(int x1, int y1, int x2, int y2) {
+		if(x1 >= sizeGrid || x2 >= sizeGrid || y1 >= sizeGrid || y2 >= sizeGrid)
+			return;
         int tmp = grid[x1][y1];
         grid[x1][y1] = grid[x2][y2];
         grid[x2][y2] = tmp;
